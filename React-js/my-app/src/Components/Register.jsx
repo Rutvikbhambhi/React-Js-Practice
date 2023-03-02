fd3e0ec66fff777a54555f2c248cad5146513b10
27JuneWMF2Rutvik <bhambhirutvik2004@gmail.com>
2023-03-02 17:02:55 +0530

    react

--- React-js/my-app/src/Components/Register.jsx
+++ React-js/my-app/src/Components/Register.jsx
@@ -1,22 +1,55 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 export default function Register(props) {
-  let btnStyle;
-  btnStyle = {
-    backgroundColor: "green",
-    color: "white",
-  };
-  let btnText,passBoxType = "Hide Password";
+  // let btnStyle;
+  // btnStyle = {
+  //   backgroundColor: "green",
+  //   color: "white",
+  // };
+  // let btnText,passBoxType = "Hide Password";
+  let btnText,passBoxType;
+  let btnClasses = ["btn", "m-1"]
   if (props.showPass === true) {
-    btnStyle.backgroundColor = "red";
+    // btnStyle.backgroundColor = "red";
     btnText = "Hide Password";
     passBoxType = "text";
+    btnClasses.push("btn-danger");
 } else {
     btnText = "Show Password"
     passBoxType = "password";
+    btnClasses.push("btn-success");
   }
+  // console.log(btnClasses);
+
+  // Style Component
+  const StyledButton = styled.button`
+    display: block;
+    padding: 10px 5px;
+    background-color: orange;
+    border: none;
+    color: white;
+    width: 100%;
+    margin: 5px;
+  `;
+  // const StyledDiv = styled.div`
+  //   background-color: red;
+  //   width: 400px;
+  //   height: 300px;
+  // `;
+
+  const StyleRegisterContainer = styled.section`
+    width: 500px;
+    &:hover {
+      box-shadow: 0px 0px 5px grey;
+    }
+    @media (min-width: 0px) and (max-width: 600px){
+      width: 300px;
+    }
+  `;
+
   return (
-    <div className="container card p-5 mt-2 register-container">
+    <StyleRegisterContainer className="container card p-5 mt-2 register-container">
       <h1 className="text-center">Registation From</h1>
       <form onSubmit={props.submit}>
         <div className="form-group">
@@ -39,10 +72,14 @@ export default function Register(props) {
         <button type="submit" className="btn btn-primary m-2">
           Register
         </button>
-        <button type="button" className="btn" onClick={props.click} style={btnStyle}>
+        {/* <button type="button" className="btn" onClick={props.click} style={btnStyle}> */}
+        <button type="button" className={btnClasses.join(" ")} onClick={props.click}>
           {btnText}
         </button>
+        <StyledButton type="button">Login</StyledButton>
+        <StyledButton type="button">T and C</StyledButton>
+        {/* <StyledDiv></StyledDiv> */}
       </form>
-    </div>
+    </StyleRegisterContainer>
   );
 }
