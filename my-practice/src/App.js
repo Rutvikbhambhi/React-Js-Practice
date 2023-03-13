ba62c2d022819b4a96742e5a6600d6098c1595b0
27JuneWMF2Rutvik <bhambhirutvik2004@gmail.com>
2023-03-13 19:04:21 +0530

    react

--- my-practice/src/App.js
+++ my-practice/src/App.js
@@ -1,145 +1,30 @@
 import "./App.css";
 import Header from "./component/Header";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Stack from "@mui/system/Stack";
-import AddIcon from "@mui/icons-material/Add";
-import { useState } from "react";
-import Fields from "./component/Fields";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { useState, useEffect } from "react";
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
+// import Stack from "@mui/system/Stack";
+// import AddIcon from "@mui/icons-material/Add";
+// import Fields from "./component/Fields";
+// import DeleteIcon from "@mui/icons-material/Delete";
 // import Movie from "./component/Movie";
 // import movies from "./movie.json";
 // import Student from "./component/Student";
 
 function App() {
-  const [form, setForm] = useState({});
-  // const [email, setEmail] = useState("");
-  const [data, setData] = useState([]);
 
-  const addData = () => {
-    setData([...data, form]);
-    setForm(form);
-    // setName("");
-    // setEmail("");
-  };
+  useEffect(() => {
+    // const []
 
-  const removeItem = (index) => {
-    let arr = data;
-    arr.splice(index, 1)
-    setData([...arr]);
-    // data.splice(index, 1);
-  }
+    console.log("useEffect Called");
+  })
 
-  // function inc(n) {
-  //   setNum(num + n);
-  // }
-
-  // function dec(n) {
-  //   setNum(num - n);
-  // }
-  // let name = "Rutvik";
-
-  // let login = false;
-
-  // if (login == false) {
-  //   return <h1>Rutvik Pramar</h1>
-  // }
+  console.log("Function Body");
 
   return (
-    <div className="App">
-      {/* {
-      login == false ? <Header /> : <div style={{color: "black"}}>Header</div>
-    } */}
-
-      {/* <h1>{name}</h1> */}
-      {/* <Student 
-        name="Rutvik" 
-        age="19"
-      /> */}
-
-      {/* {
-        (() => {
-          if(login) {
-            return <h1 style={{color: "black"}}>login</h1>
-          } else if (login == 'name') {
-
-          } else {
-            return <h1 style={{color: "black"}}>login-page</h1>
-          }
-        })()
-      } */}
-
+    <div>
       <Header />
-      <div className="form">
-        <Stack direction="row" spacing={2}>
-          <TextField
-            value={form.name}
-            onChange={(event) => setForm({...form, name: event.target.value})}
-            id="outlined-basic"
-            label="name"
-            variant="outlined"
-          />
-          <TextField
-            value={form.email}
-            onChange={(event) => setForm({...form, email: event.target.value})}
-            id="outlined-basic"
-            label="email"
-            variant="outlined"
-          />
-          <Button onClick={addData} variant="contained" color="success">
-            <AddIcon />
-          </Button>
-        </Stack>
-      </div>
-
-      <div className="data">
-        <div className="data_val">
-          <h4>Name</h4>
-          <h4>Email</h4>
-          <h4>Remove</h4>
-        </div>
-
-        {/* {
-          data.map((element, index) => {
-            return (
-              <Fields key={index} name={element.name} email={element.email} index={index} />
-            )
-          })
-        } */}
-
-        {data.map((element, index) => {
-          return (
-            <div className="data_val">
-              <h4>{element.name}</h4>
-              <h4>{element.email}</h4>
-              <Stack>
-                <Button style={{marginTop: "15px"}} onClick={() => removeItem(index)} variant="contained" color="error">
-                  <DeleteIcon />
-                </Button>
-              </Stack>
-            </div>
-          )
-        })}
-      </div>
-
-
-      {/* <div className="main">
-        <h1 className="heading">{num}</h1>
-        <div className="buttons">
-          <button className="btn" onClick={inc}>increment</button>
-          <button className="btn" onClick={dec}>decrement</button>
-        </div> */}
-      {/* {movies.map((element, index) => {
-          return (
-            <Movie
-              key={index}
-              title={element.Title}
-              year={element.Year}
-              img={element.Poster}
-            />
-          );
-        })} */}
-      {/* </div> */}
+      {console.log("Inside Jsx")}
     </div>
   );
 }
