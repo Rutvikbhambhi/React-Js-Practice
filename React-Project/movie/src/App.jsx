2d2b10c704ef3077eb45131b808d69282420353d
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-07-08 13:59:23 +0530

    project

--- React-Project/movie/src/App.jsx
+++ React-Project/movie/src/App.jsx
@@ -1,8 +1,31 @@
 
+import { useEffect } from "react";
+
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+
+// getApiConfiguration
+
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular")
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div className="App">
-      App  
+      App
     </div>
   )
 }
