deb5b011c34e6aae73c8850cd9981af78342c017
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-07-17 19:14:31 +0530

    project

--- React-Project/movie/src/Pages/explore/Explore.jsx
+++ React-Project/movie/src/Pages/explore/Explore.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import "./explore.scss"
 
 const Explore = () => {
--- React-Project/movie/src/components/header/Header.jsx
+++ React-Project/movie/src/components/header/Header.jsx
@@ -1,17 +1,71 @@
-import React from 'react'
+// import React from 'react'
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useLocation, useNavigate } from "react-router-dom";
+
 import "./header.scss";
 
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const openSearch = () => {
+    setMobileMenu(false)
+    setShowSearch(true)
+  }
+
+  const openMobileMenu = () => {
+    setMobileMenu(true)
+    setShowSearch(false)
+  }
+
   return (
-    <div className='heroBanner'>
-      <div className='wrapper'>
-        <div className="heroBannerContent">
-          <span className="title">WellCome.</span>
-          <span className="title">Millions of movies, TV shows and
-            people to discover Explore now.</span>
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
         </div>
+
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigate("")}>Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <FaSearch />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <FaSearch />
+          {mobileMenu ? (<VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+          <SlMenu onClick={openMobileMenu} />)}
+        </div>
+      </ContentWrapper>
+
+      <div className="searchBar">
+        <ContentWrapper>
+          <div className="searchInput">
+            <input 
+              type="text"
+              placeholder="Search For A Movie or TV Show"
+              onChange={(e) => setQuery(e.target.value)}
+              // onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          </div>
+        </ContentWrapper>
       </div>
-    </div>
+    </header>
   )
 }
 
