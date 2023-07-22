3d226b54c9834422d19ca7db1e993a5c9c6f8590
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-07-22 14:03:07 +0530

    Assessment

---
+++ Assessment/src/components/SearchBar.jsx
@@ -0,0 +1,45 @@
+// import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
+import "./searchBar.css";
+import { useState } from "react";
+
+const SearchBar = ({setResults}) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((Array) => {
+        const results = Array.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+        console.log(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        type="text"
+        placeholder="Type To Search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
