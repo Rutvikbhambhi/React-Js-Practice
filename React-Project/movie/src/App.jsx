0942a3305f47470393f8bdcc7b555a0ac5d472e5
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-07-22 18:13:34 +0530

    project

--- React-Project/movie/src/App.jsx
+++ React-Project/movie/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
     // apiTesting();
   }, []);
 
@@ -41,6 +42,23 @@ function App() {
       dispatch(getApiConfiguration(url));
     });
   };
+
+  const genresCall = async () => {
+    let promises = []
+    let endPoints = ["tv", "movie"]
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))  
+    })
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({genres}) => {
+      return
+    })
+
+  }
 
   return (
     <div className="App">
--- React-Project/movie/src/Pages/home/trending/Trending.jsx
+++ React-Project/movie/src/Pages/home/trending/Trending.jsx
@@ -1,10 +1,20 @@
-// import React from 'react'
+import React from 'react'
 
+import { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import Carousel from "../../../components/carousel/Carousel";
+
+import useFetch from "../../../hooks/useFetch"
 
 const Trending = () => {
-  const onTabChange = (tab) => {};
+  const [endpoint, setEndpoint] = useState("day");
+
+  const { data, loading } = useFetch(`/trending/movie/${endpoint}`)
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
 
   return (
     <div className="carouselSection">
@@ -12,6 +22,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
